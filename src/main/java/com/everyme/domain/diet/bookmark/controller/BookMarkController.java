e3ffdbeb0a37c83a5d762f3ecd446fdebbff0b47
hsh30835 <login7h@gmail.com>
2024-03-13 02:15:33 +0900

    북마크 조회 등록 삭제

--- src/main/java/com/everyme/domain/diet/bookmark/controller/BookMarkController.java
+++ src/main/java/com/everyme/domain/diet/bookmark/controller/BookMarkController.java
@@ -2,52 +2,54 @@ package com.everyme.domain.diet.bookmark.controller;
 
 import com.everyme.domain.diet.bookmark.entity.DietBookMark;
 import com.everyme.domain.diet.bookmark.service.BookMarkService;
-import com.everyme.domain.diet.dto.DietDTO;
-import com.everyme.domain.diet.entity.Diet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/dietbookmark/{dietNo}")
 public class BookMarkController {
 
     @Autowired
     private BookMarkService bookMarkService;
 
-    @PostMapping
+    @PostMapping("/registdietbm")
     public ResponseEntity<Object> registdiet(@RequestBody DietBookMark dietBookMark){
         Object result = bookMarkService.insertBookMark(dietBookMark);
-        System.out.println("북마크 컨트롤러");
 
-        if(!(result instanceof Diet)){
+        System.out.println("북마크 추가 컨트롤러");
+
+        if(!(result instanceof DietBookMark)){
             return ResponseEntity.status(404).body("등록 실패");
         }
-        Diet response = (Diet)result;
+        DietBookMark response = (DietBookMark)result;
+        System.out.println(result);
 
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping
-    public ResponseEntity<Object> getDetailDiet(@PathVariable Integer dietNo){
-        DietBookMark selectBookMark = bookMarkService.findByDietNo(dietNo);
-        if (selectBookMark != null) {
-            return ResponseEntity.ok(selectBookMark);
+
+    @GetMapping("/dietbm")
+    public ResponseEntity<Object> getDetailDiet(@RequestBody DietBookMark dietBookMark){
+        DietBookMark findBookMark = bookMarkService.findByDietNo(dietBookMark);
+
+        if (findBookMark != null) {
+            System.out.println("북마크 검색 컨트롤러");
+            return ResponseEntity.ok(findBookMark);
         } else {
             return ResponseEntity.notFound().build();
         }
     }
 
-    @DeleteMapping
-    public ResponseEntity<Object> deletediet(@PathVariable Integer dietNo){
-        Object result = bookMarkService.deleteBookMark(dietNo);
-        System.out.println("삭제");
+    @DeleteMapping("/deletedietbm")
+    public ResponseEntity<Object> deletediet(@RequestBody DietBookMark dietBookMark){
+        Object result = bookMarkService.deleteBookMark(dietBookMark);
 
-        if(!(result instanceof Diet)){
+        if(!(result instanceof DietBookMark)){
             return ResponseEntity.status(404).body("삭제 실패");
         }
 
-        Diet response = (Diet) result;
+        DietBookMark response = (DietBookMark) result;
+        System.out.println("북마크 삭제 컨트롤러");
 
         return ResponseEntity.ok(response);
     }
